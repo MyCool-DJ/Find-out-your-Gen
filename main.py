f7d2ad4892e4e7ae6f0869ab7e4e05b34ff10faf
mycooldj.eth <58099602+MyCool-DJ@users.noreply.github.com>
2022-10-04 08:39:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#day 9 of 100 days of code with Replit
+year = int (input("What year were you born? "))
+if year > 1946 and year < 1965:
+	print("The gen who complains about everything but really had it all 🙄")
+elif year > 1963 and year < 1982:
+	print("The last gen to afford a decent standard of living and maybe get a house. 🏡")
+elif year > 1981 and year < 1996:
+ print("A Millenial, Nice! Lay off the avocado 🥑, and maybe you will be able to afford a house one day!")
+elif year > 1995 and year < 2016:
+	print("The Starbucks ☕ gen ready to smash capitalism with their iMac 💻, Good Luck! No, really, it's outlived its purpose.")	
+else:
+	print("Are you a time traveller? 🕖🕠🕝🕑🕜🕐")
